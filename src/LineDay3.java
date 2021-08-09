ab98940f14c429a85fcbe62e60385901821aac95
Nikhil <Nikhilmusale70@gmail.com>
2021-08-09 15:41:08 +0530

    [Nikhil] - done with uc1

--- src/LineDay3.java
+++ src/LineDay3.java
@@ -2,6 +2,14 @@ import java.util.Random;
 
 public class LineDay3 {
     public static void main(String[] args) {
-        
+        Random ran=new Random();
+        int x1= ran.nextInt(10);
+        int y1= ran.nextInt(10);
+        int x2= ran.nextInt(10);
+        int y2= ran.nextInt(10);
+        int x= (x2-x1);
+        int y= (y2-y1);
+        double len= Math.sqrt( Math.pow(x,2) + Math.pow(y,2) );
+        System.out.println("Length of line is :- "+len);
     }
 }
