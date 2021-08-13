6227b5ef172f343384fa25c40dd450af2ffbbf5d
Nikhil Musale <nikhilmusale70@gmail.com>
2021-08-13 22:32:21 +0530

    [Nikhil] - used oops for uc4

--- src/LineDay3.java
+++ src/LineDay3.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class LineDay3 {
-    public static void main(String[] args) {
+    public static void lineDay3() {
         Random ran=new Random();
 
         //for line 1
---
+++ src/LineDay8.java
@@ -0,0 +1,5 @@
+public class LineDay8 {
+    public static void main(String[] args) {
+        LineDay3.lineDay3();
+    }
+}
